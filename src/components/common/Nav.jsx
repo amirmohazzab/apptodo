bf118c50ec717206f13b3382a428cc13f32a5967
amirmohazzab <ahm.mohazzab@gmail.com>
2022-05-26 12:46:14 +0430

    first commit

---
+++ src/components/common/Nav.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+
+    return (
+        <div className="m-3">
+        <ul className="nav nav-pills justify-content-center">
+            <li >
+            <NavLink 
+                // className={ ({ isActive }) => isActive ? "green" : "blue"}
+                to=""
+                className='nav-link'
+                exact
+                activeClassName="active"
+            >
+              Routine tasks     
+          </NavLink>
+            </li>
+            <li >
+            <NavLink 
+                // className={({ isActive }) => isActive ? "green" : "blue"}
+                to="about"
+                className='nav-link'
+            >
+              About creator
+          </NavLink>
+            </li>
+        </ul>
+    </div>
+    );
+};
+
+export default Nav;
+
